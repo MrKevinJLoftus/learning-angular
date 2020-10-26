6e35680276982622f84162a90d78387eedf892df
Kevin Loftus <mrkevinjloftus@gmail.com>
2020-10-25 22:33:15 -0400

    Subscribed to form changes

--- src/app/write-review/write-review.component.ts
+++ src/app/write-review/write-review.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-write-review',
   templateUrl: './write-review.component.html',
   styleUrls: ['./write-review.component.scss']
 })
-export class WriteReviewComponent implements OnInit {
+export class WriteReviewComponent implements OnInit, OnDestroy {
 
+  pageTitle = 'Write a Review';
   reviewForm!: FormGroup;
+  $subscription: Subscription = new Subscription();
 
   constructor(private fb: FormBuilder) {
     this.reviewForm = this.fb.group({
@@ -19,6 +22,29 @@ export class WriteReviewComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.subscribeToReviewFormChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.$subscription.unsubscribe();
+  }
+
+  /**
+   * Handle logging button click.
+   */
+  onLogButtonClick() {
+    console.log(this.reviewForm.value);
+  }
+
+  /**
+   * Subscribe to reviewForm value changes.
+   */
+  subscribeToReviewFormChanges() {
+    this.$subscription.add(this.reviewForm.valueChanges.subscribe(
+      (formValues) => {
+        console.log(formValues);
+      }
+    ));
   }
 
 }
