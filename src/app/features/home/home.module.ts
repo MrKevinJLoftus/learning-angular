873429cb4bb36d633c781707d35c1ff479fe70f2
Kevin Loftus <mrkevinjloftus@gmail.com>
2020-12-08 21:57:40 -0500

    Lazy Loading the HomeModule

---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class HomeModule { }
--- src/app/features/review/review.module.ts
+++ src/app/features/review/review.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { WriteReviewComponent } from './components/write-review/write-review.component';
 import { SharedModule } from 'src/app/shared/shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
@@ -15,7 +14,6 @@ const routes: Routes = [
     StarRatingComponent
   ],
   imports: [
-    CommonModule,
     SharedModule,
     RouterModule.forChild(routes)
   ]
